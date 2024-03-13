d7db2e554aef75eab1ba48dd2b194c399f2bc7b7
Lukas Gruber <90252420+LuGr-01@users.noreply.github.com>
2024-03-13 08:41:41 +0100

    App init

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by user935646 on 13.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
